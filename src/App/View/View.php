a1789c93758b9644dd86ba8a63ef2a77ce263101
Anastasia <belyaeva.a@500na700.ru>
2021-06-15 12:05:02 +0300

    fix view

--- src/App/View/View.php
+++ src/App/View/View.php
@@ -6,19 +6,18 @@ use App\Renderable;
 
 class View implements Renderable
 {
-    private $data;
+    private $path,
+            $parameters;
 
     public function __construct($path, $parameters)
     {
-        $this->data = [
-            'path' => VIEW_DIR . '\\' . str_replace('.', '\\', $path) . '.php',
-            'parameters' => $parameters
-        ];
+        $this->path = VIEW_DIR . '\\' . str_replace('.', '\\', $path) . '.php';
+        $this->parameters = $parameters;
     }
 
     public function render()
     {
-        $parameters = $this->data['parameters'];
-        require_once $this->data['path'];
+        $parameters = $this->parameters;
+        require_once $this->path;
     }
 }
